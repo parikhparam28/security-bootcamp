68144fc2df50aa09e2e271434f0fc6c87d6c9f94
parikhparam28 <parikhparam28@gmail.com>
2023-02-13 15:13:17 +0530

    added alert prompt and redirecting

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function createAcc(){
-    
+
 }
 
 function loginAcc(){
@@ -7,14 +7,21 @@ function loginAcc(){
     let password = document.getElementById("pass-word").value;
     console.log(emailid,password)
 
-    fetch('url_here',{
+    const data = fetch('url_here',{
         method: 'POST',
         headers: {
             'Accept' : 'application/json',
             'Content-type' : 'application/json'
         },
-        body: JSON.stringify({'id': emailid, 'password': password})
+        body: JSON.stringify({'email': emailid, 'password': password})
     })
     .then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
+    .catch((error)=> {
+        alert('Error:' + error)
+    })
+
+    if(data.response == 'success')
+        window.open('login_page_url',"_blank")
+    else 
+        alert('Wrong Credentials')
 }
